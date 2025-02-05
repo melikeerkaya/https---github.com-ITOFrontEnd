4009f10314f3ef553f15834a298eabfc5fb63449
melikeerkaya <erkayamelikee@gmail.com>
2025-02-05 11:28:40 +0300

    Add files via upload

---
+++ Odev3.sql
@@ -0,0 +1,16 @@
+
+
+select * from orders 
+where ship_country='Germany'; -- 2
+
+select * from suppliers 
+where country='Italy'; -- 4
+
+SELECT * from Products where supplier_id =14;
+
+SELECT * from customers 
+where country='UK'; -- 1
+
+select * from products 
+where category_id=2 and unit_price>20; -- 3
+
